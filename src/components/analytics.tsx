7f554474dcaf20fad18ac1df70b27b68a61fbdf0
Chirag <22btrsn010+github@jainuniversity.ac.in>
2025-04-23 12:53:26 +0530

    analytics with umami

---
+++ src/components/analytics.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from "react"
+
+export function Analytics() {
+  return (
+    <script
+      defer
+      src="https://cloud.umami.is/script.js"
+      data-website-id="dd80fdb4-1f8b-4faa-a104-73f0033f35bc"
+    />
+  )
+}
